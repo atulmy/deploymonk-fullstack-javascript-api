953c53492735070b203f2bdf8582b7c24dd8c780
atulmy <atul.12788@gmail.com>
2020-01-25 20:58:13 +0530

    CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 // Mongoose
 const DATABASE_URL = process.env.PORT || 'mongodb://localhost/fullstack'
--- routes/products.js
+++ routes/products.js
@@ -10,33 +10,9 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// Get single product
-router.get('/:id', function(req, res, next) {
-  Product.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
 // Add product
 router.post('/', function(req, res, next) {
   Product.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-// Update product
-router.put('/:id', function(req, res, next) {
-  Product.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
-
-// Delete product
-router.delete('/:id', function(req, res, next) {
-  Product.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
